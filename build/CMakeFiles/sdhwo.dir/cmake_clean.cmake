5b5d8ee154b1462a608de4491815b61b42a734c0
Chinmayee-DS <ds.chinmayee@gmail.com>
2024-10-09 01:11:25 -0500

    modified .gitignore to include all the files

---
+++ build/CMakeFiles/sdhwo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdhwo.dir/controllers/TestCtrl.cc.o"
+  "CMakeFiles/sdhwo.dir/controllers/TestCtrl.cc.o.d"
+  "CMakeFiles/sdhwo.dir/main.cc.o"
+  "CMakeFiles/sdhwo.dir/main.cc.o.d"
+  "sdhwo"
+  "sdhwo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdhwo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CMakeFiles/sdhwo_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdhwo_test.dir/test_main.cc.o"
+  "CMakeFiles/sdhwo_test.dir/test_main.cc.o.d"
+  "sdhwo_test"
+  "sdhwo_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdhwo_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
