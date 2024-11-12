b4c8004e0167a6fffcd1e6a15c4c9e757a472d97
Chinmayee-DS <ds.chinmayee@gmail.com>
2024-11-12 00:25:40 -0600

    second instance of the application

---
+++ twohwsc/controllers/controne.cc
@@ -0,0 +1,134 @@
+#include "controne.h"
+#include <drogon/HttpAppFramework.h>
+#include <stdlib.h>
+#include <mutex>
+#include <ctime>
+
+int gw = 0;
+int pw = 0;
+
+void api::v1::controne::getItem(const HttpRequestPtr &req,
+               std::function<void (const HttpResponsePtr &)> &&callback,
+               std::string key) const
+{
+    std::string value;
+    {
+        std::lock_guard<std::mutex> lock(this->mtx);
+        auto it = kv_store.find(key);
+        if(it != kv_store.end())
+        {
+            value = it->second;
+        }
+    }
+
+    if(!value.empty())
+    {
+        // gw++;
+        // std::cout << "gw " << gw << std::endl;
+        time_t timestamp;
+        time(&timestamp);
+        LOG_DEBUG << "GET " << key << " " << value << " " << ctime(&timestamp);
+
+        auto resp = HttpResponse::newHttpResponse();
+        resp->setStatusCode(k200OK);
+        resp->setBody(value);
+        callback(resp);
+    }
+    else
+    {
+        time_t timestamp;
+        time(&timestamp);
+        LOG_DEBUG << "GET " << key << " value_not_found " << ctime(&timestamp);
+
+        auto resp = HttpResponse::newHttpResponse();
+        resp->setStatusCode(k400BadRequest);
+        resp->setBody("Key not found");
+        callback(resp);
+    }
+}
+
+void api::v1::controne::putItem(const HttpRequestPtr &req,
+               std::function<void (const HttpResponsePtr &)> &&callback,
+               std::string key)
+{
+    // std::cout << "hitting the put " << std::endl;
+
+    // std::cout << "  Token: " << key << std::endl;
+    auto jo = req->getJsonObject();
+    // auto jo = req->getBody();
+    std::string val;
+    if(jo)
+    {
+        val = (*jo)["value"].asString();
+    }
+    else
+    {
+        time_t timestamp;
+        time(&timestamp);
+        LOG_DEBUG << "PUT " << key << " json_value_not_found " << ctime(&timestamp);
+
+        auto resp = HttpResponse::newHttpResponse();
+        resp->setStatusCode(k400BadRequest);
+        resp->setBody("Json unavailable");
+        callback(resp);
+    }
+    // std::cout << "value " << val << std::endl;
+    {
+        std::lock_guard<std::mutex> lock(this->mtx);
+        kv_store[key] = val;
+    }
+
+    // pw++;
+    // std::cout << "pw " << pw << std::endl;
+
+    time_t timestamp;
+    time(&timestamp);
+    LOG_DEBUG << "PUT " << key << " " << val << " " << ctime(&timestamp);
+
+    auto resp = HttpResponse::newHttpResponse();
+    resp->setStatusCode(k200OK);
+    resp->setBody("Key stored successfully");
+    callback(resp);
+}
+
+void api::v1::controne::delItem(const HttpRequestPtr &req,
+                std::function<void (const HttpResponsePtr &)> &&callback,
+                std::string key)
+{
+    std::string value;
+    {
+        std::lock_guard<std::mutex> lock(this->mtx);
+        auto it = kv_store.find(key);
+        if(it != kv_store.end())
+        {
+            value = it->second;
+            kv_store.erase(key);
+        }
+    }
+
+    if(!value.empty())
+    {
+        // gw++;
+        // std::cout << "gw " << gw << std::endl;
+
+        time_t timestamp;
+        time(&timestamp);
+        LOG_DEBUG << "DEL " << key << " " << value << " " << ctime(&timestamp);
+
+        auto resp = HttpResponse::newHttpResponse();
+        resp->setStatusCode(k200OK);
+        resp->setBody(value);
+        callback(resp);
+    }
+    else
+    {
+        time_t timestamp;
+        time(&timestamp);
+        LOG_DEBUG << "DEL " << key << " key_value_pair_not_found " << ctime(&timestamp);
+
+        auto resp = HttpResponse::newHttpResponse();
+        resp->setStatusCode(k400BadRequest);
+        resp->setBody("Key not found");
+        callback(resp);
+    }
+}
---
+++ twohwsc/main.cc
@@ -0,0 +1,13 @@
+#include <drogon/drogon.h>
+int main() {
+    //Set HTTP listener address and port
+    drogon::app().setLogPath("./").setLogLevel(trantor::Logger::kDebug);
+    drogon::app().addListener("127.0.0.2", 8081);
+    // drogon::app().addListener("0.0.0.0", 8080);
+    //Load config file
+    //drogon::app().loadConfigFile("../config.json");
+    //drogon::app().loadConfigFile("../config.yaml");
+    //Run HTTP framework,the method will block in the internal event loop
+    drogon::app().run();
+    return 0;
+}
